eb4720a14938bb082e9a5f043039b205c156b20a
mlarocca <marcellolarocca@gmail.com>
2023-10-02 14:06:57 +0200

    Queues

---
+++ python/queues/heap.py
@@ -0,0 +1,233 @@
+from typing import Any, List, Optional, Tuple
+
+def is_lower(x_1: Any, x_2: Any) -> bool:
+    """Returns True if x_1 < x_2."""
+    return x_1 < x_2
+
+class Heap:
+    """Implementation of a binary heap."""
+
+    def __init__(self, elements: List[Any] = None, is_priority_lower = is_lower) -> None:
+        """Constructor
+
+        Args:
+            elements: The elements for initializing the heap. By default, the heap is empty.
+            leq: A function that takes two elements and compares their 
+                priorities. It returns True if the first element has lower priority
+                to the second element.
+        """
+        self._lt = is_priority_lower
+        if elements is not None and len(elements) > 0:
+            self._heapify(elements)
+        else:
+            self._elements = []
+
+
+    def __len__(self) -> int:
+        """Size of the heap.
+
+        Returns: The number of elements in the heap.
+        """
+        return len(self._elements)
+
+
+    def _has_lower_priority(self, element_1: Any, element_2: Any) -> bool:
+        """Checks if the first element has lower priority to the second element."""
+        return self._lt(element_1, element_2)
+
+
+    def _has_higher_priority(self, element_1: Any, element_2: Any) -> bool:
+        """Checks if the first element has higher priority to the second element."""
+        return self._lt(element_2, element_1)
+
+
+    def _validate(self) -> bool:
+        """Checks that the three invariants for heaps are abided by.
+        1.	Every node has at most 2 children. (Guaranteed by construction)
+        2.	The heap tree is complete and left-adjusted.(Also guaranteed by construction)
+        3.	Every node holds the highest priority in the subtree rooted at that node.
+
+        Returns: True if all the heap invariants are met.
+        """
+        current_index = 0
+        first_leaf = self.first_leaf_index()
+        while current_index < first_leaf:
+            current_element: float = self._elements[current_index]
+            first_child = self._first_child_index(current_index)
+            last_child_guard = min(first_child + 2, len(self))
+            for child_index in range(first_child, last_child_guard):
+                if self._has_lower_priority(current_element,  self._elements[child_index]):
+                    return False    # pragma: no cover
+            current_index += 1
+        return True
+
+
+    def _push_down(self, index: int) -> None:
+        """Pushes down the root of a sub-heap towards its leaves to reinstate heap invariants.
+        If any of the children of the element has a higher priority, then it swaps current
+        element with its highest-priority child C, and recursively checks the sub-heap previously 
+        rooted at that C.
+
+        Args:
+            index: The index of the root of the sub-heap.
+        """
+
+        # INVARIANT: 0 <= index < n
+        assert 0 <= index < len(self._elements)
+        element = self._elements[index]
+        current_index = index
+        first_leaf = self.first_leaf_index()
+        while current_index < first_leaf:
+            child_index = self._highest_priority_child_index(current_index)
+            # INVARIANT: current_index is not a leaf, so it must have a child
+            assert child_index is not None
+            if self._has_lower_priority(element, self._elements[child_index]):
+                self._elements[current_index] = self._elements[child_index]
+                current_index = child_index
+            else:
+                break
+
+        self._elements[current_index] = element
+
+
+    def _bubble_up(self, index: int) -> None:
+        """Bubbles up towards the root an element, to reinstate heap's invariants.
+        If the parent P of an element has lower priority, then swaps current element and its parent,
+        and then recursively check the position previously held by the P.
+
+        Args:
+            index: The index of the element to bubble up.
+        """
+        # INVARIANT: 0 <= index < n
+        assert 0 <= index < len(self._elements)
+        element = self._elements[index]
+        while index > 0:
+            parent_index = self._parent_index(index)
+            parent = self._elements[parent_index]
+            if self._has_higher_priority(element, parent):
+                self._elements[index] = parent
+                index = parent_index
+            else:
+                break
+
+        self._elements[index] = element
+
+
+    def _first_child_index(self, index) -> int:
+        """Computes the index of the first child of a heap node.
+
+        Args:
+            index: The index of current node, for which we need to find children's indices.
+
+        Returns: The index of the left-most child for current heap node.
+        """
+        return index * 2 + 1
+
+
+    def _parent_index(self, index) -> int:
+        """Computes the index of the parent of a heap node.
+
+        Args:
+            index: The index of current node, for which we need to find its parent's indices.
+
+        Returns: The index of the parent of current heap node.
+
+        """
+        return (index - 1) // 2
+
+
+    def _highest_priority_child_index(self, index) -> Optional[int]:
+        """Finds, among the children of a heap node, the one child with highest priority.
+        In case multiple children have the same priority, the left-most is returned.
+
+        Args:
+            index: The index of the heap node whose children are searched.
+
+        Returns: The index of the child of current heap node with highest priority, or None if
+                 current node has no child.
+        """
+        first_index = self._first_child_index(index)
+        size = len(self)
+
+        if first_index >= size:
+            # The current element has no children
+            return None
+
+        if first_index + 1 >= size:
+            # The current element only has one child
+            return first_index
+
+        if self._has_higher_priority(self._elements[first_index], self._elements[first_index + 1]):
+            return first_index
+        else:
+            return first_index + 1
+
+
+    def first_leaf_index(self):
+        """Computes the index of the first leaf of the heap.
+           A leaf is the first node that has no children.
+           For a binary heap, we know that's exactly the node in the middle of the array.
+        """
+        return len(self) // 2
+
+
+    def _heapify(self, elements: List[Any]) -> None:
+        """Initializes the heap with a list of elements and priorities.
+
+        Args:
+            elements: The list of elements to add to the heap.
+            priorities: The priorities for those elements (in the same order they are presented).
+        """
+        self._elements = elements[:]
+        last_inner_node_index = self.first_leaf_index() - 1
+        for index in range(last_inner_node_index, -1, -1):
+            self._push_down(index)
+
+
+    def is_empty(self) -> bool:
+        """Checks if the heap is empty.
+
+        Returns: True if the heap is empty.
+
+        """
+        return len(self) == 0
+
+
+    def top(self) -> Any:
+        """Removes and returns the highest-priority element in the heap.
+        If the heap is empty, raises a `ValueError`.
+
+        Returns: The element with highest priority in the heap.
+        """
+        if self.is_empty():
+            raise ValueError('Method top called on an empty heap.')
+        if len(self) == 1:
+            element = self._elements.pop()
+        else:
+            element = self._elements[0]
+            self._elements[0] = self._elements.pop()
+            self._push_down(0)
+
+        return element
+
+
+    def peek(self) -> Any:
+        """Returns, WITHOUT removing it, the highest-priority element in the heap.
+        If the heap is empty, raises a `ValueError`.
+
+        Returns: The element with highest priority in the heap.
+        """
+        if self.is_empty():
+            raise ValueError('Method peek called on an empty heap.')
+        return self._elements[0]
+
+
+    def insert(self, element: Any) -> None:
+        """Add a new element/priority pair to the heap
+
+        Args:
+            element: The new element to add.
+            priority: The priority associated with the new element
+        """
+        self._elements.append(element)
+        self._bubble_up(len(self._elements) - 1)
---
+++ python/tests/test_heap.py
@@ -0,0 +1,104 @@
+import unittest
+import random
+
+from queues.heap import Heap
+
+class HeapTest(unittest.TestCase):
+    def test_init(self):
+        heap = Heap()
+        self.assertEqual(0, len(heap))
+
+        heap = Heap(elements=['A', 'B', 'C', 'D'])
+
+        self.assertEqual(4, len(heap))
+        self.assertTrue(heap._validate())
+
+        heap = Heap(elements=['A', 'B', 'C', 'D'], is_priority_lower=lambda x, y: x > y)
+
+        self.assertEqual(4, len(heap))
+        self.assertTrue(heap._validate())
+
+
+    def test_heapify(self):
+        size = 4 + random.randint(0, 20)
+        elements = [chr(i) for i in range(ord('A'), ord('Z'))[:size]]
+        heap = Heap(elements=elements)
+
+        self.assertEqual(size, len(heap))
+        self.assertTrue(heap._validate())
+
+
+        heap = Heap(elements=['A', 'B', 'C', 'D'])
+
+        self.assertEqual(4, len(heap))
+        self.assertEqual('D', heap.peek())
+
+        heap = Heap(elements=['A', 'B', 'C', 'D'], is_priority_lower=lambda x, y: x > y)        
+
+        self.assertEqual(4, len(heap))
+        self.assertEqual('A', heap.peek())
+
+
+    def test_heapify_duplicates(self):
+        # An heap populated with pairs, where the second element in the pair is the priority
+        heap = Heap(elements=['A', 'A', 'A', 'D', 'D'])
+
+        self.assertEqual(5, len(heap))
+        self.assertEqual('D', heap.top())
+        self.assertEqual('D', heap.top())
+        self.assertEqual('A', heap.top())
+        self.assertEqual(2, len(heap))
+
+        heap = Heap(elements=['A', 'A', 'A', 'A'])
+
+        self.assertEqual(4, len(heap))
+        self.assertEqual('A', heap.top())
+
+
+    def test_insert_top(self):
+        heap = Heap([3, 1, 4, 11, -1, 2, 10])
+        self.assertEqual(7, len(heap))
+        heap.insert(7)
+        heap.insert(5)
+        self.assertEqual(9, len(heap))
+        self.assertTrue(heap._validate())
+        heap.top()
+        heap.top()
+        heap.top()
+        self.assertEqual(6, len(heap))
+        self.assertTrue(heap._validate())
+
+        # An heap populated with pairs, where the second element in the pair is the priority
+        heap = Heap(None, is_priority_lower=lambda x, y: x[1] < y[1])
+
+        self.assertTrue(heap.is_empty())
+
+        heap.insert(('First', -1e14))
+
+        self.assertEqual('First', heap.top()[0])
+        self.assertTrue(heap.is_empty())
+
+        heap.insert(("b", 0))
+        heap.insert(("c", 0.99))
+        heap.insert(("second", 1))
+        heap.insert(("a", -11))
+
+        self.assertEqual('second', heap.top()[0])
+        self.assertEqual(3, len(heap))
+
+        for i in range(0, 10):
+            heap.insert((str(i), random.random()))
+
+        while not heap.is_empty():
+            self.assertTrue(heap._validate())
+            heap.top()
+
+
+    def test_top_empty(self):
+        heap = Heap()
+
+        with self.assertRaises(ValueError):
+            heap.top()
+
+        with self.assertRaises(ValueError):
+            heap.peek()
