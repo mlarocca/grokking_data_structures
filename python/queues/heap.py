87735eb04a0c8e85b4e089299abf1d568f592e19
mlarocca <marcellolarocca@gmail.com>
2023-10-13 01:47:04 +0200

    Nits
    Nits

--- python/queues/heap.py
+++ python/queues/heap.py
@@ -15,7 +15,7 @@ class Heap:
             element_priority: A function that extracts the priority of an element. 
                               By default, the priority is the element itself.
         """
-        self._get_priority = element_priority
+        self._priority = element_priority
         if elements is not None and len(elements) > 0:
             self._heapify(elements)
         else:
@@ -32,12 +32,12 @@ class Heap:
 
     def _has_lower_priority(self, element_1: Any, element_2: Any) -> bool:
         """Checks if the first element has lower priority to the second element."""
-        return self._get_priority(element_1) < self._get_priority(element_2)
+        return self._priority(element_1) < self._priority(element_2)
 
 
     def _has_higher_priority(self, element_1: Any, element_2: Any) -> bool:
         """Checks if the first element has higher priority to the second element."""
-        return self._get_priority(element_1) > self._get_priority(element_2)
+        return self._priority(element_1) > self._priority(element_2)
 
 
     def _validate(self) -> bool:
@@ -49,16 +49,74 @@ class Heap:
         Returns: True if all the heap invariants are met.
         """
         current_index = 0
-        first_leaf = self.first_leaf_index()
+        first_leaf = self._first_leaf_index()
         while current_index < first_leaf:
             current_element: float = self._elements[current_index]
-            first_child = self._first_child_index(current_index)
+            first_child = self._left_child_index(current_index)
             last_child_guard = min(first_child + 2, len(self))
             for child_index in range(first_child, last_child_guard):
                 if self._has_lower_priority(current_element,  self._elements[child_index]):
                     return False    # pragma: no cover
             current_index += 1
         return True
+
+
+    def _left_child_index(self, index) -> int:
+        """Computes the index of the left child of a heap node.
+
+        Args:
+            index: The index of current node, for which we need to find children's indices.
+
+        Returns: The index of the left-most child for current heap node.
+        """
+        return index * 2 + 1
+
+
+    def _parent_index(self, index: int) -> int:
+        """Computes the index of the parent of a heap node.
+
+        Args:
+            index: The index of current node, for which we need to find its parent's indices.
+
+        Returns: The index of the parent of current heap node.
+
+        """
+        return (index - 1) // 2
+
+
+    def _highest_priority_child_index(self, index: int) -> Optional[int]:
+        """Finds, among the children of a heap node, the one child with highest priority.
+        In case multiple children have the same priority, the left-most is returned.
+
+        Args:
+            index: The index of the heap node whose children are searched.
+
+        Returns: The index of the child of current heap node with highest priority, or None if
+                 current node has no child.
+        """
+        first_index = self._left_child_index(index)
+        size = len(self)
+
+        if first_index >= size:
+            # The current element has no children
+            return None #pragma no cover
+
+        if first_index + 1 >= size:
+            # The current element only has one child
+            return first_index
+
+        if self._has_higher_priority(self._elements[first_index], self._elements[first_index + 1]):
+            return first_index
+        else:
+            return first_index + 1
+
+
+    def _first_leaf_index(self):
+        """Computes the index of the first leaf of the heap.
+           A leaf is the first node that has no children.
+           For a binary heap, we know that's exactly the node in the middle of the array.
+        """
+        return len(self) // 2
 
 
     def _push_down(self, index: int) -> None:
@@ -75,7 +133,7 @@ class Heap:
         assert 0 <= index < len(self._elements)
         element = self._elements[index]
         current_index = index
-        first_leaf = self.first_leaf_index()
+        first_leaf = self._first_leaf_index()
         while current_index < first_leaf:
             child_index = self._highest_priority_child_index(current_index)
             # INVARIANT: current_index is not a leaf, so it must have a child
@@ -112,64 +170,6 @@ class Heap:
         self._elements[index] = element
 
 
-    def _first_child_index(self, index) -> int:
-        """Computes the index of the first child of a heap node.
-
-        Args:
-            index: The index of current node, for which we need to find children's indices.
-
-        Returns: The index of the left-most child for current heap node.
-        """
-        return index * 2 + 1
-
-
-    def _parent_index(self, index) -> int:
-        """Computes the index of the parent of a heap node.
-
-        Args:
-            index: The index of current node, for which we need to find its parent's indices.
-
-        Returns: The index of the parent of current heap node.
-
-        """
-        return (index - 1) // 2
-
-
-    def _highest_priority_child_index(self, index) -> Optional[int]:
-        """Finds, among the children of a heap node, the one child with highest priority.
-        In case multiple children have the same priority, the left-most is returned.
-
-        Args:
-            index: The index of the heap node whose children are searched.
-
-        Returns: The index of the child of current heap node with highest priority, or None if
-                 current node has no child.
-        """
-        first_index = self._first_child_index(index)
-        size = len(self)
-
-        if first_index >= size:
-            # The current element has no children
-            return None #pragma no cover
-
-        if first_index + 1 >= size:
-            # The current element only has one child
-            return first_index
-
-        if self._has_higher_priority(self._elements[first_index], self._elements[first_index + 1]):
-            return first_index
-        else:
-            return first_index + 1
-
-
-    def first_leaf_index(self):
-        """Computes the index of the first leaf of the heap.
-           A leaf is the first node that has no children.
-           For a binary heap, we know that's exactly the node in the middle of the array.
-        """
-        return len(self) // 2
-
-
     def _heapify(self, elements: List[Any]) -> None:
         """Initializes the heap with a list of elements and priorities.
 
@@ -178,7 +178,7 @@ class Heap:
             priorities: The priorities for those elements (in the same order they are presented).
         """
         self._elements = elements[:]
-        last_inner_node_index = self.first_leaf_index() - 1
+        last_inner_node_index = self._first_leaf_index() - 1
         for index in range(last_inner_node_index, -1, -1):
             self._push_down(index)
 
