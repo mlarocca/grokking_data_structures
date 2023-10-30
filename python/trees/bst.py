e5eb65323ec5645b2b7923bb9d9bbede74f3bbf8
mlarocca <marcellolarocca@gmail.com>
2023-10-30 18:56:18 +0100

    BST.delete

--- python/trees/bst.py
+++ python/trees/bst.py
@@ -46,7 +46,8 @@ class BinarySearchTree:
             self._right = node
 
         def find_min_in_subtree(self) -> tuple[type[BinarySearchTree.Node], type[BinarySearchTree.Node]]:
-            """Return the node with the smallest value in the subtree rooted at the node, and its parent."""
+            """Return the node with the smallest value in the subtree
+               rooted at the node, and its parent."""
             parent = None
             node = self
             while node.left() is not None:
@@ -55,7 +56,8 @@ class BinarySearchTree:
             return node, parent
 
         def find_max_in_subtree(self) -> tuple[type[BinarySearchTree.Node], type[BinarySearchTree.Node]]:
-            """Return the node with the largest value in the subtree rooted at the node, and its parent."""
+            """Return the node with the largest value in the subtree
+               rooted at the node, and its parent."""
             parent = None
             node = self
             while node.right() is not None:
@@ -90,46 +92,104 @@ class BinarySearchTree:
         return size
 
 
-    def _search(self, value: any) -> Optional[type[BinarySearchTree.Node]]:
-        """Return the first node containing the value, or None if not found.
-           If the tree contains duplicates, it returns the first node traversed 
-           that contains the target value.
+    def _search(self, value: any) -> tuple[Optional[type[BinarySearchTree.Node]], type[BinarySearchTree.Node]]:
+        """Returns a tuple.
+           The first element in the tuple is the node containing the target value,
+           or None if not found. If the tree contains duplicates, it returns the first 
+           node traversed that contains the target value.
+           The second element in the tuple is the parent of the node in the first position.
+           If the target wasn't found or if it was the root, the parent is set to None.
         """
+        parent = None
         node = self._root
         while node is not None:
             node_val = node.value()
             if node_val == value:
-                return node
+                return node, parent
             elif value < node_val:
+                parent = node
                 node = node.left()
             else:
+                parent = node
                 node = node.right()
-        return None
-    
-
-    
+        return None, None
 
 
     def contains(self, value: any) -> bool:
-        """Return True if the tree contains the value, False otherwise."""
-        return self._search(value) is not None
+        """Return True if the tree contains the value, False otherwise.
+        
+        Args:
+            value: The element to be searched in the tree.
+        Returns: True if the value is found in the tree, False otherwise.
+        """
+        return self._search(value)[0] is not None
 
 
     def insert(self, value: any) -> None:
-        """Insert a new value into the tree."""
-        parent = None
+        """Insert a new value into the tree.
+        
+        Args:
+            value: The new element to be added to the tree.
+        """
         node = self._root
+        if node is None:
+            # Empty tree
+            self._root = BinarySearchTree.Node(value)
         while node is not None:
-            parent = node
             if value <= node.value():
-                node = node.left()
+                if node.left() is None:
+                    # We have found the right spot for value
+                    node.set_left(BinarySearchTree.Node(value))
+                    break
+                else:
+                    # We keep traversing the left branch
+                    node = node.left()
+            elif node.right() is None:
+                # We have found the right spot for value
+                node.set_right(BinarySearchTree.Node(value))
+                break
             else:
+                # We keep traversing the right branch
                 node = node.right()
 
-        if parent is None:
-            self._root = BinarySearchTree.Node(value)
-        else:
-            if value <= parent.value():
-                parent.set_left(BinarySearchTree.Node(value))
+    def delete(self, value: any) -> None:
+        """ Delete a value from the tree.
+            If the value is not found, raise a ValueError.
+            If the tree is empty, raise a ValueError.
+            If the tree contains duplicates, delete the first node found.
+        Args:
+            value: The element to be deleted from the tree.
+        """
+        if self._root is None:
+            raise ValueError('Delete on an empty tree')
+        node, parent = self._search(value)
+        if node is None:
+            raise ValueError('Value not found')
+
+        if node.left() is None or node.right() is None:
+            not_none_child = node.right() if node.left() is None else node.left()
+            # The node has at most only one child
+            if parent is None:
+                # The node is the root
+                self._root = not_none_child
+            elif value <= parent.value():
+                parent.set_left(not_none_child)
             else:
-                parent.set_right(BinarySearchTree.Node(value))
+                parent.set_right(not_none_child)
+        else: # The node N has two children.
+            # Find and remove the node M with the largest value in the left subtree of N.
+            max_node, max_node_parent = node.left().find_max_in_subtree()
+            if max_node_parent is None: # M is the left child of N.
+                new_node = BinarySearchTree.Node(max_node.value(), None, node.right())
+            else:
+                new_node = BinarySearchTree.Node(max_node.value(), node.left(), node.right())
+                max_node_parent.set_right(None)
+            # Then  replace the node to be deleted with a new node with M.value(),
+            # and the same subtrees as N.
+            if parent is None:
+                # The node is the root
+                self._root = new_node
+            elif value <= parent.value():
+                parent.set_left(new_node)
+            else:
+                parent.set_right(new_node)
